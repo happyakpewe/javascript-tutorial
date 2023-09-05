9ed00d3330b8aa1e222e4bf6ada5bf1a444a4b94
samieteq <samiekeyz02@gmail.com>
2023-09-05 11:57:03 +0100

    Updated

---
+++ 9-5-2023/script.js
@@ -0,0 +1,33 @@
+let length = 7.5;
+
+let lastName = "Yellow";
+let x = true
+console.log(typeof (length) + '\n' + typeof (lastName) + '\n' + typeof (x))
+
+const cars = ["Sab", "Volvo", 12, true,]
+
+//Object
+const person = { height: 14.45, name: "Jolly", complexion: "black", country: "Nigeria" }
+console.log("array: " + cars)
+console.log('object: height - ' + person.height + ',  name - ' + person.name)
+
+
+let date = new Date()
+
+//TODO: explore DATE Object
+
+console.log(date)
+
+function add(num1, num2, num3) {
+    return num1 + num2 + num3
+}
+
+function print(result) {
+    console.log(lastName)
+    console.log(result)
+}
+
+sum = add(2, 3, 4)
+
+print(sum)
+print(add(30, 50, 12))
